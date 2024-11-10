eb3971329bb1b7a52a0b6f1135fce32be75dfc1f
david esteve <david.esteve66@gmail.com>
2024-11-10 01:18:30 +0100

    first commit structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from './views/dashboard/component/dashboard/dashboard.component';
+import {SummaryComponent} from './views/knowledge/component/summary/summary.component';
+import {AddComponent} from './views/serveur/add/add.component';
+import {EditComponent} from './views/serveur/edit/edit.component';
+import {DeleteComponent} from './views/serveur/delete/delete.component';
+import {ConfigurationComponent} from './views/utilities/configuration/configuration.component';
+import {ArticleComponent} from './views/knowledge/component/article/article.component';
+import {CreateArticleComponent} from './views/knowledge/component/create-article/create-article.component';
+import {CategoryComponent} from './views/knowledge/component/category/category.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:'dashboard', component:DashboardComponent, title:'Dashboard TuxFarm' },
+  {path:'addServeur', component:AddComponent, title:'Add Serveur TuxFarm' },
+  {path:'editServeur', component:EditComponent, title:'edit Serveur TuxFarm' },
+  {path:'deleteServeur', component:DeleteComponent, title:'delete Serveur TuxFarm' },
+  {path:'configuration', component:ConfigurationComponent, title:'configuration TuxFarm' },
+  {path:'knowledge', component:SummaryComponent},
+  {path:'category', component:CategoryComponent},
+  {path:'article', component:ArticleComponent},
+  {path:'createArticle', component:CreateArticleComponent},
+];
--- src/app/views/dashboard/component/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/component/dashboard/dashboard.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+serveurs=[
+  {
+    id: 1,
+    name: 'tux1',
+    ip: '192.168.1.1',
+    status: 'online',
+    mysqlState: 1,
+    apacheState:1,
+    charge: 25,
+    loadOverage: 1.5,
+    active:1
+  },
+  {
+    id:2,
+    name: 'tux2',
+    ip: '192.168.1.1',
+    status: 'down',
+    mysqlState: 0,
+    apacheState: 0,
+    charge: 80,
+    loadOverage: 0,
+    active:0
+  }
+]
 }
