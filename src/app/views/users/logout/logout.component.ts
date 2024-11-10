1b1604875cc8e51ebab0c3e5770e7e31e83746d8
david esteve <david.esteve66@gmail.com>
2024-11-10 01:34:04 +0100

    first commit structure

---
+++ src/app/views/users/logout/logout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tux-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.css'
+})
+export class LogoutComponent {
+
+}
